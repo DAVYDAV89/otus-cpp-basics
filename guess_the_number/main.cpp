d2fde7de5f47586877438d2d5245f0d3ff18c88c
Roman Davydov <davidov-corp@yandex.ru>
2023-07-18 09:55:59 +0300

    задает максимальное значения для загаданного числа

--- guess_the_number/main.cpp
+++ guess_the_number/main.cpp
@@ -5,10 +5,34 @@
 
 using namespace std;
 
-const int max_value = 100;
 
-int main()
+int main(int argc, char** argv)
 {
+    int max_value = 0;
+    // Let's print this argument
+    if (argc > 0) {
+        max_value = 100;
+    }
+
+    // To check - does use print some other argument we should check if the argc >= 2
+    if (argc >= 2) {
+        std::string arg1_value{ argv[1] };
+        if (arg1_value == "-parameter") {
+            std::cout << "-parameter argument was detected!" << std::endl;
+
+            // We've detected the '-parameter' argument. And we extect that after this argument there is a value:
+            int parameter_value = 0;
+            if (argc < 3) {
+                std::cout << "Wrong usage! The argument '-parameter' requires some value!" << std::endl;
+                return -1;
+            }
+            // We need to parse the string to the int value
+            max_value = std::stoi(argv[2]);
+            std::cout << "The '-parameter' value = " << max_value << std::endl;
+        }
+    }
+
+
     // Ask about name
     cout << "Hi! Enter your name, please:" << endl;
     string user_name;
