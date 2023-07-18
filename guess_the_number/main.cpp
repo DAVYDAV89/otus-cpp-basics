55eecbcece172a38347d359670250c5c19db2e31
Roman Davydov <davidov-corp@yandex.ru>
2023-07-18 10:04:08 +0300

    выводит таблицу на печать

--- guess_the_number/main.cpp
+++ guess_the_number/main.cpp
@@ -5,6 +5,55 @@
 
 using namespace std;
 
+const string high_scores_filename = "high_scores.txt";
+string user_name;
+int attempts_count = 0;
+
+void write_new_high_score()
+{
+    // We should open the output file in the append mode - we don't want
+    // to erase previous results.
+    ofstream out_file{high_scores_filename, ios_base::app};
+    if (!out_file.is_open()) {
+        cout << "Failed to open file for write: " << high_scores_filename << "!" << endl;
+        return;
+    }
+
+    // Append new results to the table:
+    out_file << user_name << ' ';
+    out_file << attempts_count;
+    out_file << endl;
+} // end of score here just to mark end of the logic block of code
+
+
+void read_the_high_score()
+{
+    ifstream in_file{high_scores_filename};
+    if (!in_file.is_open()) {
+        cout << "Failed to open file for read: " << high_scores_filename << "!" << endl;
+        return;
+    }
+
+    cout << "High scores table:" << endl;
+
+    string username;
+    int high_score = 0;
+    while (true) {
+        // Read the username first
+        in_file >> username;
+        // Read the high score next
+        in_file >> high_score;
+        // Ignore the end of line symbol
+        in_file.ignore();
+
+        if (in_file.fail()) {
+            break;
+        }
+
+        // Print the information to the screen
+        cout << username << '\t' << high_score << endl;
+    }
+}
 
 int main(int argc, char** argv)
 {
@@ -30,12 +79,15 @@ int main(int argc, char** argv)
             max_value = std::stoi(argv[2]);
             std::cout << "The '-max' value = " << max_value << std::endl;
         }
+        if (arg1_value == "-table") {
+            read_the_high_score();
+            return 0;
+        }
     }
 
 
     // Ask about name
     cout << "Hi! Enter your name, please:" << endl;
-    string user_name;
     cin >> user_name;
 
     srand(time(nullptr));
@@ -44,7 +96,7 @@ int main(int argc, char** argv)
 //    cout << "guess: " << target_value;
 
     int current_value = 0;
-    int attempts_count = 0;
+
     cout << "Enter your guess:" << endl;
 
     do {
@@ -64,52 +116,9 @@ int main(int argc, char** argv)
 
     } while(true);
 
+    write_new_high_score();
+    read_the_high_score();
 
-    const string high_scores_filename = "high_scores.txt";
-    // Write new high score to the records table
-    {
-        // We should open the output file in the append mode - we don't want
-        // to erase previous results.
-        ofstream out_file{high_scores_filename, ios_base::app};
-        if (!out_file.is_open()) {
-            cout << "Failed to open file for write: " << high_scores_filename << "!" << endl;
-            return -1;
-        }
-
-        // Append new results to the table:
-        out_file << user_name << ' ';
-        out_file << attempts_count;
-        out_file << endl;
-    } // end of score here just to mark end of the logic block of code
-
-    // Read the high score file and print all results
-    {
-        ifstream in_file{high_scores_filename};
-        if (!in_file.is_open()) {
-            cout << "Failed to open file for read: " << high_scores_filename << "!" << endl;
-            return -1;
-        }
-
-        cout << "High scores table:" << endl;
-
-        string username;
-        int high_score = 0;
-        while (true) {
-            // Read the username first
-            in_file >> username;
-            // Read the high score next
-            in_file >> high_score;
-            // Ignore the end of line symbol
-            in_file.ignore();
-
-            if (in_file.fail()) {
-                break;
-            }
-
-            // Print the information to the screen
-            cout << username << '\t' << high_score << endl;
-        }
-    }
 
     return 0;
 }
